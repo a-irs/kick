8ae8bcde2ebd9efdeb5782d9885db221982fc614
a-irs <alexirsi@gmail.com>
2016-04-04 01:19:38 +0200

    restructure

--- kick.sh
+++ kick.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 
 [[ $UID == 0 ]] || { printf "%s\n" "run as root"; exit 1; }
-source helper/configuration
+source helper/functions
+source config
+
+[[ -z "$@" ]] && { echo "usage: $0 modules/<module1> modules/<module2> ..."; exit 1; }
 
 printf "\n"
 for m in "$@"; do
