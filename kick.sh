5278f4162767149f769af6500b423bf796e3c90b
a-irs <alexirsi@gmail.com>
2016-04-04 01:10:31 +0200

    cleanup

---
+++ kick.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+[[ $UID == 0 ]] || { printf "%s\n" "run as root"; exit 1; }
+source configuration
+
+printf "\n"
+for m in "$@"; do
+    printf "$BOLD$COLOR_GREEN> running %s$RESET\n\n" "$m"
+    source "$m"
+    printf "\n"
+done
